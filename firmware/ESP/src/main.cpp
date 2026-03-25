2e5dcc35599e49370dc4ee5019e3be79c1e3b196
ocarr <ocarr@umich.edu>
2026-03-24 22:21:23 -0400

    Tinkering and Setup

---
+++ firmware/ESP/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include "esp_wifi.h"
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("ESP32-S3 ready");
+
+    //Enable WiFi Frames
+}
+
+void loop() {
+    // your code
+}
